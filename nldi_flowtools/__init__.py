92fabb9cd518f22c8aaed67d07060dfd6b2d01af
Anders <55411265+Anders-Hopkins@users.noreply.github.com>
2021-06-07 15:32:33 -0500

    Renamed nldi_flowtools to flowtool_functions, cleaned up a bit

--- nldi_flowtools/__init__.py
+++ nldi_flowtools/__init__.py
@@ -1,6 +1,6 @@
 ''' NLDI toolset to delineate drainage basins and trace raindrop paths. '''
 
-from .nldi_flowtools import splitcatchment, flowtrace
+from .flowtool_functions import splitcatchment, flowtrace
 
 from ._version import get_versions
 __version__ = get_versions()['version']

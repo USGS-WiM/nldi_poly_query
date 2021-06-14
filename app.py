91117f309954425b04f28420be42bd4aaca7ea3e
Anders <55411265+Anders-Hopkins@users.noreply.github.com>
2021-06-14 11:24:00 -0500

    add test_map.html and app.py

---
+++ app.py
@@ -0,0 +1,38 @@
+
+from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
+from nldi_flowtools.flowtool_functions import flowtrace, splitcatchment
+
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+@app.route("/")
+
+def home():
+    return "sample server"
+
+@app.route("/flowtools")
+@cross_origin(origin='*')
+def main():
+
+    lat = float(request.args.get('lat'))
+    lng = float(request.args.get('lng'))
+    runsplitcatchment = request.args.get('runsplitcatchment')
+    truefalse = bool(request.args.get('truefalse'))
+    direction = request.args.get('direction')
+    print("lat:", lat, 'lng:', lng, 'runsplitcatchment:', runsplitcatchment, 'trueflase:', truefalse, 'direction:', direction)
+
+    
+############# Splitcatchment ##############
+    if runsplitcatchment == 'true':
+        results = splitcatchment(lng, lat, truefalse)
+        print('splitcatchment results:', results)
+
+############### Flowtrace ###############
+    if runsplitcatchment == 'false':
+        results = flowtrace(lng, lat, truefalse, direction)
+        print('flowtrace results:', results)
+
+    print("results: ", type(results) , results)
+    return results
